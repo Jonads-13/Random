3fe44ef0160a324c1b71236d57b4d79d9e0d8594
Jake Jonas <jacob.jonas@student.curtin.edu.au>
2023-05-12 13:00:41 +0800

    Moved files to another repo

---
+++ printf.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class printf {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        String input3 = br.readLine();
+
+        System.out.println("================================");
+
+        String[] split1 = input1.split(" ");
+        String[] split2 = input2.split(" ");
+        String[] split3 = input3.split(" ");
+        int x = Integer.parseInt(split1[1]);
+        int y = Integer.parseInt(split2[1]);
+        int z = Integer.parseInt(split3[1]);
+        
+        System.out.printf("%-15s%03d", split1[0], x);
+        System.out.printf("\n%-15s%03d", split2[0], y);
+        System.out.printf("\n%-15s%03d\n", split3[0], z);
+
+        System.out.println("================================");
+    }
+}
